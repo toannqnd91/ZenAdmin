0234353c0e978edd649065afa692af0f7e00df0d
toannq <toannqnd@gmail.com>
2025-07-26 16:10:06 +0700

    save

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// News page logic will be added here
+</script>
+
+<template>
+  <UDashboardPanel></UDashboardPanel>
+  <div>
+    <h1>Index</h1>
+  </div>
+</template>
---
+++ app/pages/news.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// News page logic will be added here
+</script>
+
+<template>
+  <UDashboardPanel></UDashboardPanel>
+  <div>
+    <h1>News</h1>
+    <p>List of news will be displayed here.</p>
+  </div>
+</template>
