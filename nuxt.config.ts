2d7d7c8ed1b0ff8ed21f461983b9df4aebe64e79
toannq <toannqnd@gmail.com>
2025-07-30 16:52:19 +0700

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
       noExternal: ['@tinymce/tinymce-vue']
     },
     server: {
-      // host: true,
+      host: '0.0.0.0',
       allowedHosts: ['web.vnnsoft.com']
     }
   },
