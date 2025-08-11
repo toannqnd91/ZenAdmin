afb7163a7a9211d4e7061f2ed3216707dfd8e8cd
toannq <toannqnd@gmail.com>
2025-08-11 10:34:25 +0700

    save

---
+++ app/pages/orders/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Orders</h1>
+    <p>List of all orders</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Orders',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+}
+</style>
