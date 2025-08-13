0522c16c802dd288a1e572e7b1ab9e4c70b50b90
toannq <toannqnd@gmail.com>
2025-08-13 16:35:21 +0700

    [TEST] TeamsMenu

--- app/components/TeamsMenu.vue
+++ app/components/TeamsMenu.vue
@@ -1,34 +1,59 @@
 <script setup lang="ts">
-const appConfig = useAppConfig()
-const { settings } = appConfig
+import { ref, computed } from 'vue'
+import type { DropdownMenuItem } from '@nuxt/ui'
 
 defineProps<{
   collapsed?: boolean
 }>()
 
-const team = {
-  label: settings.team.label,
-  avatar: {
-    src: settings.team.avatar.src,
-    alt: settings.team.avatar.alt
+// Nếu muốn lấy từ settings/appConfig thì thay đổi đoạn này cho phù hợp
+const teams = ref([
+  {
+    label: 'Vela',
+    avatar: {
+      src: 'https://vnnsoft.com/images/vnnsoft-app-icon.png',
+      alt: 'Vela'
+    }
   }
-}
+])
+const selectedTeam = ref(teams.value[0])
 
-const router = useRouter()
-const goHome = () => {
-  router.push('/')
-}
+const items = computed<DropdownMenuItem[][]>(() => {
+  return [teams.value.map(team => ({
+    ...team,
+    onSelect() {
+      selectedTeam.value = team
+    }
+  })), [
+    {
+      label: 'Thêm website',
+      icon: 'i-lucide-circle-plus'
+    }
+  ]]
+})
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center gap-2 py-2 cursor-pointer"
-    @click="goHome">
-    <UAvatar
-      :src="team.avatar.src"
-      :alt="team.avatar.alt"
-      size="md"
+  <UDropdownMenu
+    :items="items"
+    :content="{ align: 'center', collisionPadding: 12 }"
+    :ui="{ content: collapsed ? 'w-40' : 'w-(--reka-dropdown-menu-trigger-width)' }"
+  >
+    <UButton
+      v-bind="{
+        ...selectedTeam,
+        label: collapsed ? undefined : selectedTeam?.label,
+        trailingIcon: collapsed ? undefined : 'i-lucide-chevrons-up-down'
+      }"
+      color="neutral"
+      variant="ghost"
+      block
+      :square="collapsed"
+      class="data-[state=open]:bg-elevated"
+      :class="[!collapsed && 'py-2']"
+      :ui="{
+        trailingIcon: 'text-dimmed'
+      }"
     />
-    <span v-if="!collapsed" class="font-semibold text-base">{{ team.label }}</span>
-  </div>
+  </UDropdownMenu>
 </template>
